e57f6b6f9e1584a37575a1592a8f8a771efc6554
iqbalhasanii <itsiqbalhasani@gmail.com>
2024-05-13 17:12:17 +0800

    update sketch

--- Project_13_Fan/Project_13_Fan.ino
+++ Project_13_Fan/Project_13_Fan.ino
@@ -1,24 +1,48 @@
-//#include <analogWrite.h>
+// Define the pins for the two fan motors
 #define fanPin1 19
 #define fanPin2 18
 
+// Setup function
 void setup() {
-  pinMode(fanPin1, OUTPUT);
-  pinMode(fanPin2, OUTPUT);
+  // Set fanPin1 as an output pin
+  // Set fanPin2 as an output pin
+  pinMode(fanPin1, OUTPUT); 
+  pinMode(fanPin2, OUTPUT); 
 }
 
+// Loop function
 void loop() {
-  digitalWrite(fanPin1, LOW); //pwm = 0
-  analogWrite(fanPin2, 180);
-  delay(3000);
-  digitalWrite(fanPin1, LOW);
-  digitalWrite(fanPin2, LOW);
-  delay(1000);
-  digitalWrite(fanPin1, HIGH); //pwm = 255
-  analogWrite(fanPin2, 210);
-  delay(3000);
-  digitalWrite(fanPin1, LOW);
-  digitalWrite(fanPin2, LOW);
-  delay(1000);
-
+  // Turn off fanPin1 and 
+  // set fanPin2 to 70% duty cycle (180/255)
+  // Turn off fanPin1
+  // Set fanPin2 to 70% duty cycle
+  // Wait for 3 seconds
+  digitalWrite(fanPin1, LOW);   
+  analogWrite(fanPin2, 180);     
+  delay(3000);                    
+  
+  // Turn off both fanPin1 and fanPin2
+  // Turn off fanPin1
+  // Turn off fanPin2
+  // Wait for 1 second
+  digitalWrite(fanPin1, LOW);    
+  digitalWrite(fanPin2, LOW);    
+  delay(1000);                    
+  
+  // Turn on fanPin1 and 
+  // set fanPin2 to 82% duty cycle (210/255)
+  // Turn on fanPin1
+  // Set fanPin2 to 82% duty cycle
+  // Wait for 3 seconds
+  digitalWrite(fanPin1, HIGH);   
+  analogWrite(fanPin2, 210);     
+  delay(3000);                    
+  
+  // Turn off both fanPin1 and fanPin2
+  // Turn off fanPin1
+  // Turn off fanPin2
+  // Wait for 1 second
+  digitalWrite(fanPin1, LOW);    
+  digitalWrite(fanPin2, LOW);    
+  delay(1000);                    
 }
