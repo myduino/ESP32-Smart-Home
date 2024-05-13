4eea536018e7d53bdb2445695190de3ba25a9e2b
iqbalhasanii <itsiqbalhasani@gmail.com>
2024-05-13 15:50:01 +0800

    update sketch

--- Project_10_Servo_Auto_Window/Project_10_Servo_Auto_Window.ino
+++ Project_10_Servo_Auto_Window/Project_10_Servo_Auto_Window.ino
@@ -1,26 +1,51 @@
+//please install the ESP32_Servo.h library
 #include <ESP32_Servo.h>
-Servo myservo;
-#define servoPin 5
-#define waterPin 34
 
+// Create a servo object to control a servo
+// Define the GPIO pin connected to the servo
+// Define the GPIO pin connected to the water sensor
+Servo myservo;        
+#define servoPin 5    
+#define waterPin 34   
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(waterPin, INPUT);
-  myservo.attach(servoPin);
-  myservo.write(176);
-  delay(200);
+// The setup function to run each code once
+// from top to bottom within the setup function,
+// once executed last code it exits the
+// setup function, and continue to function loop
+void setup() { 
+  // Initialize serial communication at 9600 baud rate
+  // Set the water sensor pin as INPUT
+  // Attach the servo to the specified pin
+  // Move the servo to the initial position (176 degrees)
+  // Delay to allow the servo to reach the initial position
+  Serial.begin(9600);        
+  pinMode(waterPin, INPUT);  
+  myservo.attach(servoPin);  
+  myservo.write(176);        
+  delay(200);                
 }
 
 void loop() {
-  int water_val = analogRead(waterPin);
-  Serial.println(water_val);
+  // Read the water sensor value
+  // Print the water sensor value to serial monitor
+  int water_val = analogRead(waterPin); 
+  Serial.println(water_val);            
+
+  // Check if the water level is above the threshold (1500 in this case)
   if(water_val > 1500) {
-    myservo.write(0);
-    delay(200);
-  }
-  else {
-    myservo.write(176);
-    delay(200);
+    // If the water level is above the threshold,
+    // move the servo to close the valve
+    // Command the servo to move to the closed position (0 degrees)
+    // Delay to allow the servo to reach the closed position
+    myservo.write(0);    
+    delay(200);          
+
+  } else {
+    // If the water level is below the threshold, 
+    // move the servo to open the valve
+    // Command the servo to move to the open position (176 degrees)
+    // Delay to allow the servo to reach the open position
+    myservo.write(176);  
+    delay(200);          
   }
 }
